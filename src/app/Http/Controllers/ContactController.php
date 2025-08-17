928b96ba724d28849ce5d1d60d7f91aae53e4e69
megumi <megumi.chocolate1209@gmail.com>
2025-08-17 18:33:32 +0900

    Add confirm flow: controller, routing, and view updates

--- src/app/Http/Controllers/ContactController.php
+++ src/app/Http/Controllers/ContactController.php
@@ -11,8 +11,9 @@ class ContactController extends Controller
         return view('index');
     }
 
-    public function confirm()
+    public function confirm(Request $request)
     {
-        // ここに処理を記述していきます。
+        $contact = $request->only(['name', 'email', 'tel', 'content']);
+        return view('confirm',compact('contact'));
     }
 }
